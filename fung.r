176b6ffea52a7e9f08dcb39144430b9f6e2a76bf
agelinne <gelinne.aaron@gmail.com>
2020-02-15 14:23:07 -0500

    survival plot 2/15

--- fung.r
+++ fung.r
@@ -3,7 +3,8 @@ install.packages("pacman")
 library(pacman)
 p_load(pacman, dplyr, GGally, ggplot2, ggthemes, 
        ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, 
-       stringr, tidyr, MASS, oddsratio, finalfit, grid,survival,survminer,plyr) 
+       stringr, tidyr, MASS, oddsratio, finalfit, grid,
+       gridExtra, survival,survminer,plyr) 
 
 #START HERE
 #import fungal stats
@@ -18,7 +19,7 @@ for (i in colnames(df[,35:37])){
   df[[i]] <- factor(df[[i]],levels = c("0","1"),labels=c("No","Yes"))  
 }
 
-#sink(file="output.txt")
+sink(file="output.txt")
 #Summary tables
 df_sum <- df[, -grep(".f$", colnames(df))]
 explanatory = c(colnames(df_sum[,3:20]))
@@ -33,22 +34,56 @@ finalfit(df,dependent, explanatory, metrics=TRUE, na_to_missing = TRUE)
 
 ##Survival Plot
 #convert variables to proper format for surv fitting
-#df$td.asperg <- ifelse(df$td.asperg == 999,240,df$td.asperg)
 df$td.asperg <- replace_na(df$td.asperg, 240)
+df$td.asperg <- ifelse(df$td.asperg == 999,NA,df$td.asperg)
 
 df$e <- ifelse(df$exp.asper == "Yes","1","0")
 df$e <- as.numeric(df$e)
 
-#Survival fitting
-surv_object <- Surv(time=df$td.asperg, event = df$e)
-fit1 <- survfit(surv_object ~ df$surg, data = df)
-summary(fit1)
-#survival plot
-#tiff("surv.tiff", units="in", width=5, height=5, res=300)
-ggsurvplot(fit1, data = df, pval = TRUE,
-           xlab = "Time (days)", ylab = "Survival probability",
-           conf.int=TRUE, font.x=c(12,"bold"), font.y=c(12,"bold"), risk.table = TRUE,
-           ggtheme = theme_bw(), break.time.by = 50, palette=c("#808080", "#2E9FDF"))
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#Survival plots
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
+require("survival")
+vczf <- survfit( Surv(td.asperg, e) ~ vcz,
+                 data = df )
+itzf <- survfit( Surv(td.asperg, e) ~ itz,
+                 data = df )
+surgf <- survfit( Surv(td.asperg, e) ~ surg,
+                 data = df )
+
+require("survminer")
+splots <- list()
+
+splots[[1]] <- ggsurvplot(vczf, data = df, risk.table = FALSE, 
+                          xlab = element_blank(), ylab = "",
+                          xlim = c(0,240), ylim=c(0.6,1), 
+                          pval.coord = c(1,.65),pval = TRUE, pval.size = 2,
+                          conf.int=TRUE, font.x=c(12,"bold"), font.y=c(12,"bold"),
+                          legend.title = "VCZ",legend.labs = c("No","Yes"), legend = c("right"),
+                          ggtheme = theme_bw(), break.time.by = 30, palette=c("#808080", "#2E9FDF"))
+
+splots[[2]] <- ggsurvplot(itzf, data = df, risk.table = FALSE, 
+                          xlab = element_blank(), ylab = "Survival probability",
+                          xlim = c(0,240), ylim=c(0.6,1), 
+                          pval.coord = c(1,.65), pval = TRUE,pval.size = 2,
+                          conf.int=TRUE, font.x=c(12,"bold"), font.y=c(12,"bold"),
+                          legend.title = "ITZ",legend.labs = c("No","Yes"), legend = c("right"),
+                          ggtheme = theme_bw(), break.time.by = 30, palette=c("#808080", "#2E9FDF"))
+
+splots[[3]] <- ggsurvplot(surgf, data = df, risk.table = FALSE,
+                          xlab = "Time (days)", ylab = "",
+                          xlim = c(0,240), ylim=c(0.6,1), 
+                          pval.coord = c(1,.65), pval = TRUE,pval.size = 2,
+                          conf.int=TRUE, font.x=c(12,"bold"), font.y=c(12,"bold"),
+                          legend.title = "Surgery",legend.labs = c("No","Yes"), legend = c("right"),
+                          ggtheme = theme_bw(), break.time.by = 30, palette=c("#808080", "#2E9FDF"))
+
+# Arrange multiple ggsurvplots and print the output
+tiff("surv.tiff", units="in", width=5, height=5, res=300)
+arrange_ggsurvplots(splots,
+                    ncol = 1, nrow = 3)
+dev.off()
 
 #clean up
 rm(list = ls())
