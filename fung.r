ab19211f01a2feaf8f7d3dc71324f29654b1287f
agelinne <gelinne.aaron@gmail.com>
2020-02-15 09:12:13 -0500

    2/14

--- fung.r
+++ fung.r
@@ -18,14 +18,13 @@ for (i in colnames(df[,35:37])){
   df[[i]] <- factor(df[[i]],levels = c("0","1"),labels=c("No","Yes"))  
 }
 
-sink(file="output.txt")
+#sink(file="output.txt")
 #Summary tables
 df_sum <- df[, -grep(".f$", colnames(df))]
 explanatory = c(colnames(df_sum[,3:20]))
 dependent = 'exp.asper'
 summary_factorlist(df,dependent, explanatory,
                    p=TRUE, add_dependent_label=TRUE)
-
 
 #Regression Table dysphagia
 explanatory = c("surg","vcz","ampb", "lampb","itz","fluco")
@@ -34,7 +33,8 @@ finalfit(df,dependent, explanatory, metrics=TRUE, na_to_missing = TRUE)
 
 ##Survival Plot
 #convert variables to proper format for surv fitting
-df$td.asperg <- ifelse(df$td.asperg == 999,NA,df$td.asperg)
+#df$td.asperg <- ifelse(df$td.asperg == 999,240,df$td.asperg)
+df$td.asperg <- replace_na(df$td.asperg, 240)
 
 df$e <- ifelse(df$exp.asper == "Yes","1","0")
 df$e <- as.numeric(df$e)
@@ -44,7 +44,7 @@ surv_object <- Surv(time=df$td.asperg, event = df$e)
 fit1 <- survfit(surv_object ~ df$surg, data = df)
 summary(fit1)
 #survival plot
-tiff("surv.tiff", units="in", width=5, height=5, res=300)
+#tiff("surv.tiff", units="in", width=5, height=5, res=300)
 ggsurvplot(fit1, data = df, pval = TRUE,
            xlab = "Time (days)", ylab = "Survival probability",
            conf.int=TRUE, font.x=c(12,"bold"), font.y=c(12,"bold"), risk.table = TRUE,
